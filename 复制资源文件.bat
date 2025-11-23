c600ced311c29a2f5b35eb9ac2052612d1878eda
面包屑 <1198383781w@gmail.com>
2025-11-23 18:19:20 +0800

    Add files via upload

---
+++ 复制资源文件.bat
@@ -0,0 +1,9 @@
+@echo off
+chcp 65001 >nul
+echo 正在复制资源文件...
+copy "..\job3\guzheng.mp3" "guzheng.mp3"
+copy "..\job3\guzheng.png" "guzheng.png"
+copy "..\job3\play.mp4" "play.mp4"
+echo 资源文件复制完成！
+pause
+
